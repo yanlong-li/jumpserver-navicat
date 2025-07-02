63ad0097b312858a63262af2f5f1e1c149cd1fe4
Yanlongli <ahlyl94@gmail.com>
2025-07-02 18:21:56 +0800

    移除因控制台GBK编码问题引入的encoding_rs库，释放二进制文件大小147KB

--- src/regedit.rs
+++ src/regedit.rs
@@ -1,12 +1,12 @@
 use std::env;
-use std::fs::{File, remove_file};
+use std::fs::{remove_file, File};
 use std::process::Command;
 
 use byteorder::{LittleEndian, WriteBytesExt};
-use encoding_rs::GBK;
 
 pub fn get_reg_str() -> String {
-    String::from(r#"Windows Registry Editor Version 5.00
+    String::from(
+        r#"Windows Registry Editor Version 5.00
 
 [HKEY_CLASSES_ROOT\jms]
 @="URL:jms Protocol"
@@ -19,41 +19,56 @@ pub fn get_reg_str() -> String {
 [HKEY_CLASSES_ROOT\jms\shell\open\command]
 @="\"{{program}}\" \"%1\""
 
-"#)
+"#,
+    )
 }
 
 pub fn detecting() -> bool {
-
-    let output = Command::new("reg").arg("query").arg("HKEY_CLASSES_ROOT\\jms\\shell\\open\\command").output().expect("查询失败");
-
+    // 修改命令行编码
+    Command::new("cmd")
+        .arg("/C")
+        .arg("chcp")
+        .arg("65001")
+        .output()
+        .expect("<UNK>");
+    let output = Command::new("reg")
+        .arg("query")
+        .arg("HKEY_CLASSES_ROOT\\jms\\shell\\open\\command")
+        .output()
+        .expect("查询失败");
 
     let mut ok = false;
 
     if output.status.success() {
         let binding = output.stdout.to_vec();
-        let out_msg = GBK.decode(&*binding);
+        let out_msg = String::from_utf8(binding).unwrap();
         // println!("ok {:?}", out_msg.0);
         let program = env::current_exe().expect("获取路径失败");
 
         let s = program.to_str().unwrap();
 
-        ok = out_msg.0.contains(&s);
+        ok = out_msg.contains(&s);
 
         if !ok {
             println!("jms:// 协议地址不匹配，重新注册")
         }
     } else {
         let binding = output.stderr.to_vec();
-        let out_msg = GBK.decode(&*binding);
-        println!("err {:?}", out_msg.0);
+        let out_msg = String::from_utf8(binding);
+        println!("err {:?}", out_msg);
     }
-
 
     ok
 }
 
 pub fn register() -> bool {
-    let root_path = env::current_exe().expect("获取当前路径失败").parent().expect("获取父级目录").to_str().expect("转换为字符串失败").to_string();
+    let root_path = env::current_exe()
+        .expect("获取当前路径失败")
+        .parent()
+        .expect("获取父级目录")
+        .to_str()
+        .expect("转换为字符串失败")
+        .to_string();
 
     let program = env::current_exe().expect("获取路径失败");
 
@@ -79,23 +94,42 @@ pub fn register() -> bool {
         .output()
         .expect("导入注册表失败");
 
-
     // 将输出转换为字符串
 
     if output.status.success() {
         let binding = output.stdout.to_vec();
-        let out_msg = GBK.decode(binding.as_ref());
-        println!("{:?}", out_msg);
+        println!("{:?}", String::from_utf8(binding));
     } else {
         let binding = output.stderr.to_vec();
-        let out_msg = GBK.decode(binding.as_ref());
-        println!("{:?}", out_msg.0);
+        println!("{:?}", String::from_utf8(binding));
     }
-
 
     // 删除文件
     let res = remove_file(reg_path);
     println!("清理临时数据 {:?}", res);
 
     output.status.success()
+}
+
+#[cfg(test)]
+mod tests {
+    use std::process::Command;
+
+    #[test]
+    fn it_works() {
+        Command::new("cmd")
+            .arg("/C")
+            .arg("chcp")
+            .arg("65001")
+            .output()
+            .expect("<UNK>");
+        let output = Command::new("reg")
+            .arg("query")
+            .arg("HKEY_CLASSES_ROOT\\jms\\shell\\open\\command")
+            .output()
+            .expect("查询失败");
+
+        // let out_msg = UTF_8.decode(&output.stdout);
+        println!("{:?}", String::from_utf8(output.stdout));
+    }
 }
